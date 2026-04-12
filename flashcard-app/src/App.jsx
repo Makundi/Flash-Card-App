32551c969e404e7510607bf2909e4a3ab79f03f8
kelvin <makundykelvin@gmail.com>
2026-04-12 14:13:08 +0300

    app responsiveness added

--- flashcard-app/src/App.jsx
+++ flashcard-app/src/App.jsx
@@ -20,68 +20,60 @@ function App() {
       ans: "Making advanced weapons",
     },
   ];
-  const [progress, setProgress] = useState(1);
   const [question, setQuestion] = useState(0);
   const [show, setShow] = useState(false);
-
-  const handleNextPrevButtons = (action) => {
-    if (question >= questions.length || question < 0) {
-      return;
-    }
-    if (action === "next") {
-      setProgress(progress + 1);
-      setQuestion(question + 1);
-    }
-    else {
-      setProgress(progress - 1);
-      setQuestion(question - 1);
-    }
-  };
 
   return (
     <>
       <h1>Flash Cards</h1>
-      <div class="card">
-        <div class="container">
+      <div className="card">
+        <div className="container">
           <div
-            class="progress-bar"
-            style={{ width: Math.floor(((progress) / questions.length) * 100) + "%" }}
+            className="progress-bar"
+            style={{ width: `${Math.floor((question / questions.length) * 100)}%` }}
           >
-            {Math.floor(((progress) / questions.length) * 100) + '%'}
+            <span>{Math.floor(((question) / questions.length) * 100) + '%'}</span>
           </div>
         </div>
-        <div class="question-card">
-          <div class="question-section">{questions[question].qn}</div>
-          <div class="button-section">
+        <div className="question-card">
+          <div className="question-section">{question < questions.length ? questions[question].qn : "End"}</div>
+          <div className="button-section">
             <button 
               id="prev-btn" 
-              class={progress > 1 ? "btn active" : "btn"}
-              onClick={() => handleNextPrevButtons('prev')}
-              disabled = {progress > 1 ? false : true}
+              className={question > 0 ? "btn active" : "btn"}
+              onClick={() => {
+                setQuestion(question-1)
+                setShow(false)
+              }}
+              disabled = {question > 0 ? false : true}
             >
               Previous
             </button>
             <button
               id="show-hide-btn"
-              class="show-hide"
+              className="show-hide"
               onClick={() => setShow(!show)}
+              disabled={question < questions.length ? false : true}
             >
               {show ? "Hide Answer" : "Show Answer"}
             </button>
             <button
               id="next-btn"
-              class={progress < questions.length ? "btn active" : "btn"}
-              onClick={() => handleNextPrevButtons("next")}
-              disabled={progress < questions.length ? false : true}
+              className={question < questions.length ? "btn active" : "btn"}
+              onClick={() => {
+                setQuestion(question+1)
+                setShow(false)
+              }}
+              disabled={question < questions.length ? false : true}
             >
               Next
             </button>
           </div>
         </div>
-        <div class="scene">
-          <div class={show ? "answer-card show" : "answer-card"}>
-            <div class="answer-section front">?</div>
-            <div class="answer-section back">{questions[question].ans}</div>
+        <div className="scene">
+          <div className={show ? "answer-card show" : "answer-card"}>
+            <div className="answer-section front">{question < questions.length ? "?" : "End"}</div>
+            <div className="answer-section back">{question < questions.length ? questions[question].ans : ""}</div>
           </div>
         </div>
       </div>
